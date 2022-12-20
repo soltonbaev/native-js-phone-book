b27a31c1cbbbd2790268e7ffd70a20d6ba516d05
soltonbaev <isoltonbaev@gmail.com>
2022-12-20 23:58:53 +0600

    reworked some rendering

--- index.js
+++ index.js
@@ -17,14 +17,18 @@
 При сдаче прикрепить ссылку на гитхаб (zip, stackblitz и тд не будут приняты) */
 
 async function setJSON(contact, id) {
+  // console.log("Updating JSON server...");
+  let endpoint = "http://localhost:8000/contacts/";
   if (contact === "delete" && id) {
+    console.log("Deleting a contact...");
     const options = {
       method: "DELETE",
     };
-    fetch(`http://localhost:7000/contacts/${id}`, options);
+    await fetch(`${endpoint}${id}`, options);
+    await renderNames();
   } else if (id === undefined) {
+    console.log("Creating a new contact...");
     [name, lastName, phone, photo] = contact;
-
     const contactObj = {
       name: name,
       lastName: lastName,
@@ -38,8 +42,10 @@ async function setJSON(contact, id) {
       },
       body: JSON.stringify(contactObj),
     };
-    fetch("http://localhost:7000/contacts", options);
+    await fetch(endpoint, options);
+    await renderNames();
   } else if (contact && id) {
+    console.log("Updating a contact...");
     [name, lastName, phone, photo] = contact;
     const contactObj = {
       name: name,
@@ -54,12 +60,14 @@ async function setJSON(contact, id) {
       },
       body: JSON.stringify(contactObj),
     };
-    fetch(`http://localhost:7000/contacts/${id}`, options);
+    await fetch(`${endpoint}${id}`, options);
+    await renderNames();
+    await renderContact(id, contactObj);
   }
 }
 
 async function getJSON() {
-  let contactsResponse = await fetch("http://localhost:7000/contacts");
+  let contactsResponse = await fetch("http://localhost:8000/contacts");
   result = await contactsResponse.json();
   return result;
 }
@@ -69,8 +77,8 @@ function grabForms() {
   let inputLName = document.getElementById("forms_last-name");
   let inputPhone = document.getElementById("forms_phone");
   let inputPhoto = document.getElementById("forms_photo");
-  let inputBtn = document.getElementById("forms_btn");
-  inputBtn.addEventListener("click", () => {
+  let inputBtn = document.getElementById("forms_btn-add");
+  inputBtn.addEventListener("click", function addNew() {
     if (inputPhoto.value == "") {
       inputPhoto.value = "./assets/placeholder.jpg";
     }
@@ -84,88 +92,74 @@ function grabForms() {
 }
 
 async function renderNames() {
+  console.log("Rendering list of contacts...");
   let names = document.getElementById("names");
+  names.innerText = "";
   let contacts = await getJSON();
   contacts.forEach((contact) => {
     let li = document.createElement("li");
     names.append(li);
     li.innerText = `${contact.name} ${contact.lastName}`;
     li.addEventListener("click", () => {
-      renderContact(contact.id);
+      renderContact(contact.id, contact);
     });
   });
 }
-async function renderContact(id) {
-  let renderedUl = document.getElementById("contacts_rendered-ul");
+async function renderContact(id, contact) {
   let renderedName = document.getElementById("contacts_rendered-name");
   let renderedLName = document.getElementById("contacts_rendered-last-name");
   let renderedPhone = document.getElementById("contacts_rendered-phone");
   let renderedPhoto = document.getElementById(
     "contacts_rendered-photo-wrapper"
   );
-  let contacts = await getJSON();
-  contacts.forEach((contact) => {
-    if (id === contact.id) {
-      console.log(contact);
-      renderedName.innerText = contact.name;
-      renderedLName.innerText = contact.lastName;
-      renderedPhone.innerText = contact.phone;
-      renderedPhoto.innerHTML = `<img class = "contacts_photo" src="${contact.photo}">`;
-    }
-  });
-  editContacts(id);
-  deleteContact(id);
+
+  if (id === contact.id) {
+    renderedName.innerText = contact.name;
+    renderedLName.innerText = contact.lastName;
+    renderedPhone.innerText = contact.phone;
+    renderedPhoto.innerHTML = `<img class = "contacts_photo" src="${contact.photo}">`;
+  }
+  editContact(id, contact);
+  await deleteContact(id);
 }
 
-function deleteContact(id) {
+async function deleteContact(id) {
   let del = document.getElementById("delete");
-  del.addEventListener("click", (e) => {
-    setJSON("delete", id);
+
+  del.addEventListener("click", async function del() {
+    console.log("Clicking delete button...");
+    await setJSON("delete", id);
   });
+  del.removeEventListener("click", del);
 }
 
-function editContacts(id) {
-  let renderedName = document.getElementById("contacts_rendered-name");
-  let renderedLName = document.getElementById("contacts_rendered-last-name");
-  let renderedPhone = document.getElementById("contacts_rendered-phone");
-  let renderedPhotoWrapper = document.getElementById(
-    "contacts_rendered-photo-wrapper"
-  );
-  let renderedImg = document.getElementById("contacts_photo");
-  let renderedUL = document.getElementById("contacts_rendered-ul");
-  let save = document.getElementById("save");
-  let cancel = document.getElementById("cancel");
-  let dataContainer = renderedUL.querySelectorAll(".contacts_data-container");
-  dataContainer.forEach((li) => {
-    let editInp = document.createElement("input");
-    li.addEventListener("click", () => {
-      editInp.value = li.innerText;
-      li.parentNode.replaceChild(editInp, li);
-      if (editInp.value == "") {
-        editInp.value == "add new value";
-      }
-      editInp.focus();
-    });
-    editInp.addEventListener("focusout", () => {
-      li.innerText = editInp.value;
-    });
-    cancel.addEventListener("click", () => {
-      editInp.parentNode.replaceChild(li, editInp);
-    });
-  });
-
-  save.addEventListener("click", () => {
-    let contact = [
-      renderedName.textContent,
-      renderedLName.textContent,
-      renderedPhone.textContent,
-      renderedPhoto.textContent,
-    ];
-
-    setJSON(contact, id);
+function editContact(id, contact) {
+  let inputName = document.getElementById("forms_name");
+  let inputLName = document.getElementById("forms_last-name");
+  let inputPhone = document.getElementById("forms_phone");
+  let inputPhoto = document.getElementById("forms_photo");
+  let editSpan = document.getElementById("forms_edit-span");
+  let inputBtn = document.getElementById("forms_btn-add");
+  inputBtn.classList.add("toggle");
+  let updateBtn = document.getElementById("forms_btn-update");
+  editSpan.classList.remove("toggle");
+  inputName.value = contact.name;
+  inputLName.value = contact.lastName;
+  inputPhone.value = contact.phone;
+  inputPhoto.value = contact.photo;
+  updateBtn.innerText = "Update";
+  updateBtn.addEventListener("click", async function update() {
+    if (inputPhoto.value == "") {
+      inputPhoto.value = "./assets/placeholder.jpg";
+    }
+    await setJSON(
+      [inputName.value, inputLName.value, inputPhone.value, inputPhoto.value],
+      id
+    );
+    // updateBtn.removeEventListener("click", update);
   });
 }
 
 grabForms();
 renderNames();
-renderContact(1);
+// renderContact(1, null);
