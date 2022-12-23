6184d1355b9957dc136d26058639816e81a9f426
soltonbaev <isoltonbaev@gmail.com>
2022-12-23 22:04:25 +0600

    edit

--- index.js
+++ index.js
@@ -1,126 +1,103 @@
-const API = "https://api.jsonbin.io/v3/b/63a5834501a72b59f2378af6";
+// ====================DECLARE GLOBALS======================
+const API = "https://sltnbv-json-server.herokuapp.com/contacts";
 
+let inputName = document.getElementById("forms_name");
+let inputLName = document.getElementById("forms_last-name");
+let inputPhone = document.getElementById("forms_phone");
+let inputPhoto = document.getElementById("forms_photo");
+let addBtn = document.getElementById("forms_btn-add");
+let contactList = document.getElementById("names");
+let renderedName = document.getElementById("contacts_rendered-name");
+let renderedLName = document.getElementById("contacts_rendered-last-name");
+let renderedPhone = document.getElementById("contacts_rendered-phone");
+let renderedPhoto = document.getElementById("contacts_rendered-photo-wrapper");
+let editSpan = document.getElementById("forms_edit-span");
+let updateBtn = document.getElementById("forms_btn-update");
+let delBtn = document.getElementById("delete");
+let newContact = document.getElementById("new");
+let contactDetails = document.getElementById("contact-details");
+let contactInstructions = document.getElementById("contact-instructions");
+let list = document.getElementById("names");
+
+//===================LAUNCH APP========================
 async function launchContactBook() {
-  let ui = grabUiElements();
-  await renderNames(ui);
-  await launchListeners(ui);
+  await renderNames();
+  await launchListeners();
 }
-function grabUiElements() {
-  let inputName = document.getElementById("forms_name");
-  let inputLName = document.getElementById("forms_last-name");
-  let inputPhone = document.getElementById("forms_phone");
-  let inputPhoto = document.getElementById("forms_photo");
-  let inputBtn = document.getElementById("forms_btn-add");
-  let contactList = document.getElementById("names");
-  let renderedName = document.getElementById("contacts_rendered-name");
-  let renderedLName = document.getElementById("contacts_rendered-last-name");
-  let renderedPhone = document.getElementById("contacts_rendered-phone");
-  let renderedPhoto = document.getElementById(
-    "contacts_rendered-photo-wrapper"
-  );
-  let editSpan = document.getElementById("forms_edit-span");
-  let updateBtn = document.getElementById("forms_btn-update");
-  let del = document.getElementById("delete");
-  let newContact = document.getElementById("new");
-  let contactDetails = document.getElementById("contact-details");
-  let contactInstructions = document.getElementById("contact-instructions");
-  let list = document.getElementById("names");
-  return {
-    inpName: inputName,
-    inpLName: inputLName,
-    inpPhone: inputPhone,
-    inpPhoto: inputPhoto,
-    add: inputBtn,
-    contactList: contactList,
-    name: renderedName,
-    lName: renderedLName,
-    phone: renderedPhone,
-    photo: renderedPhoto,
-    editSpan: editSpan,
-    update: updateBtn,
-    delete: del,
-    new: newContact,
-    details: contactDetails,
-    instructions: contactInstructions,
-    list: list,
-  };
-}
-
-async function launchListeners(ui, id, type) {
+//==================LAUNCH EVENT LISTENERS=====================
+async function launchListeners(id, type) {
   // add new contact
-  if (ui && id && type === "new") {
-    ui.new.addEventListener("click", function newContact() {
-      ui.editSpan.classList.add("hidden");
-      ui.add.classList.remove("hidden");
-      ui.details.classList.add("hidden");
-      ui.instructions.classList.remove("hidden");
+  if (id && type === "new") {
+    newContact.addEventListener("click", function clearContact() {
+      editSpan.classList.add("hidden");
+      addBtn.classList.remove("hidden");
+      contactDetails.classList.add("hidden");
+      contactInstructions.classList.remove("hidden");
       let currentLi = document.getElementById(id);
       currentLi.classList.remove("selected");
-      clearInputs(ui);
-      ui.new.removeEventListener("click", newContact);
+      clearInputs();
+      newContact.removeEventListener("click", clearContact);
     });
   } else if (id && type === "update") {
     let newUpdateBtn = document.createElement("button");
     newUpdateBtn.innerText = "Update";
     newUpdateBtn.setAttribute("id", "forms_btn-update");
     newUpdateBtn.classList.add("contacts_forms");
-    ui.editSpan.prepend(newUpdateBtn);
+    editSpan.prepend(newUpdateBtn);
     newUpdateBtn.addEventListener("click", async function update() {
-      await updateContact(ui, id);
-      await renderNames(ui);
+      await updateContact(id);
+      await renderNames();
     });
   } else if (id && type == "delete") {
-    ui.delete.addEventListener("click", async function del() {
+    delBtn.addEventListener("click", async function del() {
       await setJSON("delete", id);
-      ui.delete.removeEventListener("click", del);
-      await renderNames(ui);
-      await renderContact(ui);
+      delBtn.removeEventListener("click", del);
+      await renderNames();
+      await renderContact();
     });
   } else {
-    ui.add.addEventListener("click", function addNew() {
-      if (ui.inpPhoto.value == "") {
-        ui.inpPhoto.value = "./assets/placeholder.jpg";
+    addBtn.addEventListener("click", function addNew() {
+      if (inputPhoto.value == "") {
+        inputPhoto.value = "./assets/placeholder.jpg";
       }
-      setJSON([
-        ui.inpName.value,
-        ui.inpLName.value,
-        ui.inpPhone.value,
-        ui.inpPhoto.value,
-      ]);
-      renderNames(ui);
-      clearInputs(ui);
+      setJSON(
+        [inputName.value, inputLName.value, inputPhone.value, inputPhoto.value],
+        id
+      );
+
+      clearInputs();
     });
   }
 }
 
-async function renderNames(ui) {
-  ui.contactList.innerText = ""; // clear contact list
+async function renderNames() {
+  contactList.innerText = ""; // clear contact list
   let contacts = await getJSON();
   console.log(contacts);
   contacts.forEach((contact) => {
     let li = document.createElement("li");
-    ui.contactList.append(li);
+    contactList.append(li);
     li.innerText = `${contact.name} ${contact.lastName}`;
     li.setAttribute("id", contact.id);
     li.addEventListener("click", async function render() {
-      await renderContact(ui, contact.id, contact);
+      await renderContact(contact.id, contact);
     });
   });
 }
 
-async function updateContact(ui, id) {
-  if (ui.inpPhoto.value == "") {
-    ui.inpPhoto.value = "./assets/placeholder.jpg";
+async function updateContact(id) {
+  if (inputPhoto.value == "") {
+    inputpPhoto.value = "./assets/placeholder.jpg";
   }
   await setJSON(
-    [ui.inpName.value, ui.inpLName.value, ui.inpPhone.value, ui.inpPhoto.value],
+    [inputName.value, inputLName.value, inputPhone.value, inputPhoto.value],
     id
   );
 }
 
-async function renderContact(ui, id, contact) {
+async function renderContact(id, contact) {
   if (id && contact && id === contact.id) {
-    let liCol = ui.list.querySelectorAll("li");
+    let liCol = contactList.querySelectorAll("li");
     let currentLi = document.getElementById(id);
     currentLi.classList.add("selected");
     liCol.forEach((li) => {
@@ -130,44 +107,43 @@ async function renderContact(ui, id, contact) {
     });
     let update = document.getElementById("forms_btn-update");
     update.remove();
-    ui.details.classList.remove("hidden");
-    ui.instructions.classList.add("hidden");
-    ui.name.innerText = contact.name;
-    ui.lName.innerText = contact.lastName;
-    ui.phone.innerText = contact.phone;
-    ui.photo.innerHTML = `<img class = "contacts_photo" src="${contact.photo}">`;
-    await editContact(ui, id, contact);
-    launchListeners(ui, id, "delete");
-    launchListeners(ui, id, "new");
+    contactDetails.classList.remove("hidden");
+    contactInstructions.classList.add("hidden");
+    renderedName.innerText = contact.name;
+    renderedLName.innerText = contact.lastName;
+    renderedPhone.innerText = contact.phone;
+    renderedPhoto.innerHTML = `<img class = "contacts_photo" src="${contact.photo}">`;
+    await editContact(id, contact);
+    launchListeners(id, "delete");
+    launchListeners(id, "new");
   } else {
-    clearInputs(ui);
+    clearInputs();
   }
 }
 
-function clearInputs(ui) {
-  ui.inpName.value = "";
-  ui.inpLName.value = "";
-  ui.inpPhone.value = "";
-  ui.inpPhoto.value = "";
+function clearInputs() {
+  inputName.value = "";
+  inputLName.value = "";
+  inputPhone.value = "";
+  inputPhoto.value = "";
 }
 
-async function editContact(ui, id, contact) {
-  ui.add.classList.add("hidden");
-  ui.editSpan.classList.remove("hidden");
-  ui.inpName.value = contact.name;
-  ui.inpLName.value = contact.lastName;
-  ui.inpPhone.value = contact.phone;
-  ui.inpPhoto.value = contact.photo;
-  await launchListeners(ui, id, "update");
+async function editContact(id, contact) {
+  addBtn.classList.add("hidden");
+  editSpan.classList.remove("hidden");
+  inputName.value = contact.name;
+  inputLName.value = contact.lastName;
+  inputPhone.value = contact.phone;
+  inputPhoto.value = contact.photo;
+  await launchListeners(id, "update");
 }
 
 async function setJSON(contact, id) {
-  let endpoint = API;
   if (contact === "delete" && id) {
     const options = {
       method: "DELETE",
     };
-    await fetch(`${endpoint.record}${id}`, options);
+    await fetch(`${API}/${id}`, options);
   } else if (id === undefined) {
     [name, lastName, phone, photo] = contact;
     const contactObj = {
@@ -183,7 +159,8 @@ async function setJSON(contact, id) {
       },
       body: JSON.stringify(contactObj),
     };
-    await fetch(endpoint, options);
+    await fetch(API, options);
+    renderNames();
   } else if (contact && id) {
     [name, lastName, phone, photo] = contact;
     const contactObj = {
@@ -199,15 +176,15 @@ async function setJSON(contact, id) {
       },
       body: JSON.stringify(contactObj),
     };
-    await fetch(`${endpoint}${id}`, options);
+    await fetch(`${API}/${id}`, options);
   }
 }
 
 async function getJSON() {
   let contactsResponse = await fetch(API);
   result = await contactsResponse.json();
-  let resArr = result.record;
-  return resArr;
+
+  return result;
 }
 
 launchContactBook();
